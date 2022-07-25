6b4b3784c44412c9735a1652620856cd520b195c
DmitriyDmitrishin <dmitrishin52@gmail.com>
2022-07-25 18:24:03 +0500

    123

--- src/test/java/config/Credentials.java
+++ src/test/java/config/Credentials.java
@@ -3,7 +3,7 @@ package config;
 import org.aeonbits.owner.ConfigFactory;
 
 public class Credentials {
-    public static CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
+    public static CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class, System.getProperties());
 
     public static boolean isRemoteWebDriver() {
         return !config.remote().equals("");
