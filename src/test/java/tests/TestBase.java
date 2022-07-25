e396b106b0691d01b738f0964dd1a1ad92554429
DmitriyDmitrishin <dmitrishin52@gmail.com>
2022-07-25 18:18:27 +0500

    123

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -29,7 +29,7 @@ public class TestBase {
         @BeforeEach
         void openAppliedTech() {
             step("open appliedtech", () -> {
-                open("https://www.appliedtech.ru/");
+                open("");
             });
         }
 
