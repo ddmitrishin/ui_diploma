6e1b0f93e1df153c677ac815db915edcf05c979c
DmitriyDmitrishin <dmitrishin52@gmail.com>
2022-07-25 18:41:12 +0500

    123

--- src/test/java/config/CredentialsConfig.java
+++ src/test/java/config/CredentialsConfig.java
@@ -3,7 +3,7 @@ package config;
 import org.aeonbits.owner.Config;
 
 @Config.Sources({
-        "classpath:credentials.properties"})
+        "classpath:properties/credentials.properties"})
 public interface CredentialsConfig extends Config {
     String user();
     String password();
